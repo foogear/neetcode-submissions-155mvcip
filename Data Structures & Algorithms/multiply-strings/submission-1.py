f00451130752b34a20d714898165672981d7c369
foogear <neetcode.io.likely662@passmail.com>
2026-04-16 00:06:45 +0800

    Add: multiply-strings - submission-1

---
+++ Data Structures & Algorithms/multiply-strings/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+
+
+
+        def strToInt(s):
+            s = s[::-1]
+            map = {"0":0, "1":1, "2":2, "3":3, "4":4,
+                   "5":5, "6":6, "7":7, "8":8, "9":9}
+            res = 0
+            for i in range(len(s)):
+                res += map[s[i]] * (10 ** i)
+            return res
+
+        return str(strToInt(num1) * strToInt(num2))
+
+
+
