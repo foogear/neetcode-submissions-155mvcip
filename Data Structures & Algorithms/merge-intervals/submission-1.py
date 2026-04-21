028f99fea207f50ef355591256e230a0dc6ae1a2
foogear <neetcode.io.likely662@passmail.com>
2026-04-21 12:41:56 +0800

    Add: merge-intervals - submission-1

---
+++ Data Structures & Algorithms/merge-intervals/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+
+        intervals = sorted(intervals)
+        
+        if len(intervals) < 2:
+            return intervals
+
+        res = []
+        newInterval = intervals[0]
+
+        for i in range(1, len(intervals)):
+
+            if newInterval[1] < intervals[i][0]:
+                res.append(newInterval)
+                newInterval = intervals[i]
+                continue
+
+            leftEnd, rightStart = newInterval[1], intervals[i][0]
+            if leftEnd >= rightStart:
+                newInterval = [newInterval[0], intervals[i][1]]
+
+        res.append(newInterval)
+        return res
+
+
+
+
+
+
