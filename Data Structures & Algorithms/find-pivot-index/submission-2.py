c9ca80368d935defb8e8bc4975c51ab6be5541cc
foogear <neetcode.io.likely662@passmail.com>
2026-05-12 12:55:55 +0800

    Add: find-pivot-index - submission-2

---
+++ Data Structures & Algorithms/find-pivot-index/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+
+        l = 0
+        r = sum(nums)
+
+        for i in range(len(nums)):
+            r -= nums[i]
+            if l == r:
+                return i
+            l += nums[i]
+
+        return -1
+
