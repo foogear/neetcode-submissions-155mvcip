ee85cc347ccd8371456543e4c0d0af2046963a08
foogear <neetcode.io.likely662@passmail.com>
2026-05-16 19:30:33 +0800

    Add: design-word-search-data-structure - submission-2

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-2.py
@@ -0,0 +1,44 @@
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.endOfWord = True
+
+    def search(self, word: str) -> bool:
+
+        def dfs(word, i = 0, curr = self.root):
+            # if i == len(word) - 1: # BUG
+            if i == len(word):
+                if curr.endOfWord:
+                    return True 
+                return False
+
+            if word and not curr.children: # BUG
+                return False
+
+            c = word[i]
+            if c == '.':
+                for k in curr.children:
+                    if not dfs(word, i + 1, curr.children[k]):
+                        return False 
+            elif c not in curr.children:
+                return False
+            elif not dfs(word, i + 1, curr.children[c]):
+                return False
+
+            return True
+
+        return dfs(word)
+
+class TrieNode:
+
+    def __init__(self) -> None:
+        self.children = {}
+        self.endOfWord = False
