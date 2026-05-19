02293b777f6a4db3e631448e1490f9b9c6c4c3d5
foogear <neetcode.io.likely662@passmail.com>
2026-05-19 19:02:30 +0800

    Add: count-connected-components - submission-3

---
+++ Data Structures & Algorithms/count-connected-components/submission-3.py
@@ -0,0 +1,40 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        parent = [p for p in range(n)]
+        # UnboundLocalError
+        # components = n
+        components = [n]
+
+        def findRoot(x):
+            xp = x
+            while xp != parent[xp]:
+                xp = parent[xp]
+                parent[x] = xp
+            return xp
+
+        def union(x, y):
+            rx = findRoot(x)
+            ry = findRoot(y)
+
+            if rx == ry:
+                return False
+
+            # UnboundLocalError
+            # components -= 1
+            components[0] -= 1
+
+            # BUG
+            # parent[y] = rx 
+            parent[ry] = rx
+            return True 
+
+        for e in edges:
+            union(e[0], e[1])
+
+        return components[0]
+        
+
+
+
+
+
