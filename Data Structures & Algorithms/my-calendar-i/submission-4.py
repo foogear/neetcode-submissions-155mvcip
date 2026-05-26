792da221c44cece951310dca508955538b48e5e5
foogear <neetcode.io.likely662@passmail.com>
2026-05-26 10:13:27 +0800

    Add: my-calendar-i - submission-4

---
+++ Data Structures & Algorithms/my-calendar-i/submission-4.py
@@ -0,0 +1,60 @@
+class MyCalendar:
+    
+    def __init__(self):
+        self.root = TreeNode(None, None, 0, 0)
+        
+    def book(self, startTime: int, endTime: int) -> bool:
+        # print()
+        # print()
+        # print(f"inset{(startTime,endTime)}")
+        def insert(startTime, endTime, root):
+            if not root:
+                # print("++")
+                # print((startTime, endTime))
+                canInsert[0] = True
+                return TreeNode(None, None, startTime, endTime)
+
+            # print("*********")
+            # print(f"parent{(root.startTime, root.endTime)}")
+
+            # cantInsert = ((root.startTime <= startTime < root.endTime)
+                       # or (root.startTime <    endTime < root.endTime))
+            cantInsert = not(
+                root.endTime <= startTime
+                or endTime <= root.startTime
+            )
+            if cantInsert:
+                # print("stop here")
+                canInsert[0] = False
+                # return None BUG
+                return root
+            if root.endTime <= startTime:
+                # print('r')
+                root.rightChild = insert(startTime, endTime, root.rightChild)
+                # return root.rightChild
+                return root
+            elif endTime <= root.startTime:
+                # print('l')
+                root.leftChild  = insert(startTime, endTime, root.leftChild)
+                # return root.leftChild
+                return root
+
+        canInsert = [False]
+        insert(startTime, endTime, self.root)
+
+        if canInsert[0]:
+            return True
+        else:
+            return False
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(startTime,endTime)
+
+class TreeNode:
+    def __init__(self, lc, rc, st, et) -> None:
+        self.leftChild  = lc
+        self.rightChild = rc
+        self.startTime = st
+        self.endTime = et
+
