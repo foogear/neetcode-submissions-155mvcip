6d6f648ab04d9c1d7c18d7947a83c8bcac9b0b25
foogear <neetcode.io.likely662@passmail.com>
2026-05-30 16:39:12 +0800

    Add: remove-duplicates-from-sorted-array-ii - submission-0

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array-ii/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        L = 0
+        currGroup = nums[L]
+        # count = 1
+        count = 0
+
+        # for R in range(1, len(nums)):
+        for R in range(0, len(nums)):
+            if nums[R] == currGroup:
+                if count >= 2:
+                    count += 1
+                    continue
+            else:
+                currGroup = nums[R]
+                count = 0
+
+            nums[L], nums[R] = nums[R], nums[L]
+            count += 1
+            L += 1
+
+        return L
+
+
