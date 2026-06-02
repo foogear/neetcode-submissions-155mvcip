1d4cfca0b569fc67e4a5c7958e5dd5f7562375af
foogear <neetcode.io.likely662@passmail.com>
2026-06-02 19:38:02 +0800

    Add: combinations-of-a-phone-number - submission-1

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-1.py
@@ -0,0 +1,35 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        ncMap = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"],
+        }
+
+        def combination(digits, digitsLen = len(digits), i = 0):
+
+            if not digits:
+                return []
+            if i == len(digits) - 1:
+                return ncMap[digits[i]]
+
+            res = []
+            for l in ncMap[digits[i]]:
+                for r in combination(digits, digitsLen, i + 1):
+
+                    res.append([l, r])
+
+            return res
+
+        resList = combination(digits)
+        res = []
+        for l in resList:
+            res.append("".join(l))
+
+        return res
+
