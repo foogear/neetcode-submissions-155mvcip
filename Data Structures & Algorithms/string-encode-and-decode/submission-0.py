1c7935275b0f80228914edc4c39cedde7cda09b6
foogear <neetcode.io.likely662@passmail.com>
2026-06-06 19:40:19 +0800

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encStrs = []
+        for s in strs:
+            encStrs.append(s)
+            encStrs.append("/#*#/")
+
+        encStrs = "".join(encStrs)
+        return encStrs
+
+    def decode(self, s: str) -> List[str]:
+        separator = "/#*#/"
+        I = 0
+        decList, t = [], []
+        while I < len(s):
+            if (s[I + 0] == separator[0]
+            and s[I + 1] == separator[1]
+            and s[I + 2] == separator[2]
+            and s[I + 3] == separator[3]
+            and s[I + 4] == separator[4]):
+                decList.append("".join(t))
+                t = []
+                I += 5
+                continue
+
+            t.append(s[I])
+            I += 1
+
+        return decList
+
