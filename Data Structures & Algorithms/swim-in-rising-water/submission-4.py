4e2d3c4b7a44d1be7c2a809967f777155fa37a7f
foogear <neetcode.io.likely662@passmail.com>
2026-06-13 12:18:19 +0800

    Add: swim-in-rising-water - submission-4

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-4.py
@@ -0,0 +1,50 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        adj = []
+        for i in range(len(grid)):
+            adj.append([])
+            for j in range(len(grid[0])):
+                adj[i].append([])
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                left  = grid[i][j - 1] - grid[i][j] if j - 1 > -1 else None
+                right = grid[i][j + 1] - grid[i][j] if j + 1 < len(grid[0]) else None
+                up    = grid[i - 1][j] - grid[i][j] if i - 1 > -1 else None
+                down  = grid[i + 1][j] - grid[i][j] if i + 1 < len(grid) else None
+                if left  != None:
+                    cost = 0 # left  if left  > 0 else 0
+                    adj[i][j].append((cost, (i, j - 1)))
+                if right != None:
+                    cost = 0 # right if right > 0 else 0
+                    adj[i][j].append((cost, (i, j + 1)))
+                if up    != None:
+                    cost = 0 # up if up > 0 else 0
+                    adj[i][j].append((cost, (i - 1, j)))
+                if down  != None:
+                    cost = 0 # down if down > 0 else 0
+                    adj[i][j].append((cost, (i + 1, j)))
+
+        costMap = {}
+        currLevel = 0
+        # minCostHeap = [(       0, (0,0))] # BUG
+        minCostHeap = [(grid[0][0], (0,0))]
+        end = (len(grid) - 1, len(grid[0]) - 1)
+        while minCostHeap and (end not in costMap):
+            currCost, curr = heapq.heappop(minCostHeap)
+            if curr in costMap:
+                continue
+            costMap[curr] = currCost
+            currLevel = max(currLevel, grid[curr[0]][curr[1]])
+
+            for nextCost, next in adj[curr[0]][curr[1]]:
+                if next in costMap:
+                    continue
+
+                if grid[next[0]][next[1]] <= currLevel:
+                    nextCost = 0
+                else:
+                    nextCost = grid[next[0]][next[1]] - currLevel
+                heapq.heappush(minCostHeap, (currCost + nextCost, next))
+
+        return costMap[end]
+
