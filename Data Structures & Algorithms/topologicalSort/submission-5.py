93056e77b89f2dd13c0e4f702a7bc8d8f92d102b
foogear <neetcode.io.likely662@passmail.com>
2026-06-16 13:10:07 +0800

    Add: topologicalSort - submission-5

---
+++ Data Structures & Algorithms/topologicalSort/submission-5.py
@@ -0,0 +1,38 @@
+class Solution:
+    def topologicalSort(self, n: int, edges: List[List[int]]) -> List[int]:
+        visited = set()
+        currPath = set()
+        topological = []
+
+        adj = []
+        for l in range(n): # in case of
+            adj.append([]) # independence vertex
+        for l, r in edges:
+            adj[l].append(r)
+
+        def dfs(start):
+            if start in visited: # has to check first
+                return True 
+            if start in currPath:
+                return False
+            currPath.add(start)
+
+            # if start in adj: not using map now
+            for r in adj[start]:
+                # if r not in visited:
+                if not dfs(r):
+                    return False
+
+            currPath.remove(start)
+            topological.append(start)
+            visited.add(start)
+
+            return True
+
+        for start in range(n):
+            if not dfs(start):
+                return []
+
+        topological.reverse()
+        return topological
+
