3ce66762543c58af279d4da9a3ccfa7f1a906f1b
foogear <neetcode.io.likely662@passmail.com>
2026-07-07 14:38:16 +0800

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        L, R = 0, 0
+
+        for R in range(len(prices)):
+            if prices[L] >= prices[R]:
+                L = R
+            else:
+                profit = max(profit, prices[R] - prices[L])
+
+        return profit
+
+
