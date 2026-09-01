7fc0ba3f4beb5a4bec26f3b1918e363d5fcb296b
foogear <neetcode.io.likely662@passmail.com>
2026-09-01 19:39:15 +0800

    Add: evaluate-reverse-polish-notation - submission-0

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for t in tokens:
+            if t == '+':
+                stack.append(stack.pop() + stack.pop())
+            elif t == '-':
+                stack.append(stack.pop() - stack.pop())
+            elif t == '*':
+                stack.append(stack.pop() * stack.pop())
+            elif t == '/':
+                stack.append(stack.pop() / stack.pop())
+            else:
+                stack.append(int(t))
+
+        return stack.pop()
