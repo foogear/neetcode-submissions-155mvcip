8b456f452eebd4bbbad623294e3b801af450cbfa
foogear <neetcode.io.likely662@passmail.com>
2026-09-13 11:30:45 +0800

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if not edges:
+            if n == 1:
+                return True
+            return False
+
+        # undirected Tree should consider as GROUP not ROOT
+
+        vertexGroup = {}
+
+        for v1, v2 in edges:
+            g1 = vertexGroup.get(v1, None)
+            g2 = vertexGroup.get(v2, None)
+            if not g1 and not g2:
+                groupID = 1 # Tree should only have 1 group
+                vertexGroup[v1] = groupID
+                vertexGroup[v2] = groupID
+            elif not g1:
+                vertexGroup[v1] = vertexGroup[v2]
+            elif not g2:
+                vertexGroup[v2] = vertexGroup[v1]
+            else: # default がなければ未知な条件網羅できないから
+                if vertexGroup[v1] == vertexGroup[v2]:
+                    return False
+        
+        if n == len(vertexGroup):
+            return True
+            
+        return False
