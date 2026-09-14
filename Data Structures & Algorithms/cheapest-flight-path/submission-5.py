2c3356cd6b748e1cb748ef09ca34fde09a40fe62
foogear <neetcode.io.likely662@passmail.com>
2026-09-14 11:17:41 +0800

    Add: cheapest-flight-path - submission-5

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-5.py
@@ -0,0 +1,46 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+
+        print()
+        adjMap = {}
+        for i in range(n):
+            adjMap[i] = []
+        for start, end, cost in flights:
+            adjMap[start].append((cost, end))
+
+        dstCost = [10000]
+
+        def dfs(start, cost, step, visited = set()):
+            #print("\n\n")
+            #print((start, cost, step))
+            #print (visited)
+            if start in visited or step > k + 1:
+                return
+            visited.add(start)
+
+
+
+            # if step == k + 1: ❌
+            # if step <= k + 1: ⭕
+            if start == dst:
+                dstCost[0] = min(dstCost[0], cost)
+                #print("cost update")
+                visited.remove(start)
+                return 
+
+            for nextCost, next in adjMap[start]:
+                dfs(next, nextCost + cost, step + 1, visited)
+
+
+            visited.remove(start)
+
+    
+
+
+        dfs(src, 0, 0)
+        if dstCost[0] != 10000:
+            return dstCost[0]
+        
+        return -1####
+
+
